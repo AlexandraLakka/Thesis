5127599f12f3944573cad97a77ff21402b75182b
AlexandraLakka <34437741+AlexandraLakka@users.noreply.github.com>
2022-01-31 04:10:20 +0200

    Add files via upload

--- aws_functions/quick/quick.py
+++ aws_functions/quick/quick.py
@@ -1,17 +1,8 @@
-from algorithms.sort import merge_sort,quick_sort
-#from prometheus_client import start_http_server
+from algorithms.sort import quick_sort
 import random
-import time
-from sys import getsizeof
 
-if __name__ == "__main__":
-    #start_http_server(8000)
-    start_time = time.time()
-
-    my_list = [random.randrange(1, 2000000, 1) for i in range(2000000)]
-    #my_list = merge_sort(my_list)
+def handler(event, context):
+    my_list = [random.randrange(1, 2000000, 1) for i in range(100000)]
     my_list = quick_sort(my_list)
     print(my_list)
 
-    print("--- Size of input: %s" % (getsizeof(my_list)))
-    print("--- Execution time: %s seconds ---" % (round(time.time() - start_time, 2)))
