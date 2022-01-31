22e041518e5ab785d54d128096adafe9c0304264
AlexandraLakka <34437741+AlexandraLakka@users.noreply.github.com>
2022-01-31 04:07:44 +0200

    Add files via upload

--- aws_functions/cholesky/cholesky.py
+++ aws_functions/cholesky/cholesky.py
@@ -1,17 +1,12 @@
 from algorithms.matrix import cholesky_matrix_decomposition
-#from prometheus_client import start_http_server
 import numpy as np
 import random
-from sys import getsizeof
-import time
 
-if __name__ == "__main__":
-    #start_http_server(8000)
-    start_time = time.time()
+def handler(event, context):
 
     #A = [random.sample(range(-20000, 20000), 1050) for _ in range(1050)]
-    matrixSize1 = 1050
-    matrixSize2 = 1050
+    matrixSize1 = 450
+    matrixSize2 = 450
     A = np.random.rand(matrixSize1, matrixSize2)
     B = np.dot(A, A.transpose())
     print(B)
@@ -19,6 +14,3 @@ if __name__ == "__main__":
     V = cholesky_matrix_decomposition.cholesky_decomposition(B)
 
     print(V)
-
-    print("---Size of input matrix: %s bytes---" % (getsizeof(B)))
-    print("--- Execution time: %s seconds ---" % (round(time.time() - start_time, 2)))
