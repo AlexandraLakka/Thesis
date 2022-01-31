9f0f546b0cecbfc48e1454eba3c80fe68816b32e
AlexandraLakka <34437741+AlexandraLakka@users.noreply.github.com>
2022-01-31 04:09:29 +0200

    Add files via upload

--- aws_functions/multiplication/multiply.py
+++ aws_functions/multiplication/multiply.py
@@ -1,20 +1,11 @@
 from algorithms.matrix import multiply
-#from prometheus_client import start_http_server
-from sys import getsizeof
-import time
 import random
 
-if __name__ == "__main__":
-    #start_http_server(8000)
-    start_time = time.time()
-    A = [random.sample(range(2000), 1050) for _ in range(1050)]
-    B = [random.sample(range(2000), 1050) for _ in range(1050)]
+def handler(event, context):
+    A = [random.sample(range(2000), 450) for _ in range(450)]
+    B = [random.sample(range(2000), 450) for _ in range(450)]
 
     C = multiply.multiply(A, B)
     print(C)
 
-    print("--- Size of A: %s bytes ---" % (getsizeof(A)))
-    print("--- Size of B: %s bytes ---" % (getsizeof(B)))
-    input = getsizeof(A) + getsizeof(B)
-    print("--- Size of input: %s bytes" % (input))
-    print("--- Execution time: %s seconds ---" % (round(time.time() - start_time, 2)))
+
